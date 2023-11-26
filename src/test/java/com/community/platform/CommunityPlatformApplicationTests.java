d671c15d2bffcbbbce480b43bba44835c7aefc3b
khoon9 <kshun08@dongguk.edu>
2023-11-27 02:22:30 +0900

    chore: #4-스프링 부트 프로젝트 시작

---
+++ src/test/java/com/community/platform/CommunityPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
