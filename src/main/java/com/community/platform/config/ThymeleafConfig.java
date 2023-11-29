25e4d37652269cf3c41b002e7d6e503800405375
khoon9 <kshun08@dongguk.edu>
2023-11-29 23:49:48 +0900

    chore: #14 - ThymeleafConfig 설정
    마크다운 문서와 thymeleaf 문서 분리를 위한 설정

---
+++ src/main/java/com/community/platform/config/ThymeleafConfig.java
@@ -0,0 +1,24 @@
+package com.community.platform.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(true);
+
+        return defaultTemplateResolver;
+    }
+
+
+}
